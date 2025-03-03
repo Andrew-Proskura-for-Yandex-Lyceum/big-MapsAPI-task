02250f88f11fd5efddd20b800f6e2657e33f3aab
Andrew-Proskura <andrewproskura3@gmail.com>
2025-03-03 11:35:57 +0300

    Первая задача. Работает, если я правильно понял, о котором из масштабов говорится в условии

---
+++ window.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+
+import pygame
+import requests
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('coords')
+    parser.add_argument('scale')
+    args = parser.parse_args()
+    server_address = 'https://static-maps.yandex.ru/v1?'
+    api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
+    ll = args.coords
+    scale = args.scale
+
+    map_request = f"{server_address}"
+    params = {'ll': ll, 'z': scale, 'apikey': api_key}
+    response = requests.get(map_request, params=params)
+
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
+
+    os.remove(map_file)
+
+
+if __name__ == '__main__':
+    main()
